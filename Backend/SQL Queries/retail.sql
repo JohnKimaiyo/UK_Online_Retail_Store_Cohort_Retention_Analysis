e0544e04ad2eef4d6d92aa1a901ca13609582982
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-18 12:07:45 +0300

    final commit

--- Backend/SQL Queries/retail.sql
+++ Backend/SQL Queries/retail.sql
@@ -8,3 +8,9 @@ SELECT TOP (1000) [InvoiceNo]
       ,[CustomerID]
       ,[Country]
   FROM [UK Online Retail Store].[dbo].['UK Online Retail$']
+
+  -- let get rid of missing vlaues in customer ID --
+    SELECT *
+
+  FROM [UK Online Retail Store].[dbo].['UK Online Retail$']
+  WHERE [CustomerID] ! = '';
